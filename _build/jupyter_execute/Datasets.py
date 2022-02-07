2aa7d4bdc00cf52a7f35f27b60a7704890cadbbb
Andreas Gerbl <65550853+AndreasGerbl@users.noreply.github.com>
2022-02-07 07:17:18 +0100

    update

---
+++ _build/jupyter_execute/Datasets.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Datasets
+# 
+# 
+# Regression --> Housing.csv (Github) </br>
+# Classification --> ParisHousingClass.csv (Github)</br>
+# Clustering --> Spending_Score.csv (Github)</br>
+# ## What is MyST?
