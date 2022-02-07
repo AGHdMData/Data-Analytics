89ce3dc02f5e57cebbc316fe89937f1cffbb1d9e
Andreas Gerbl <65550853+AndreasGerbl@users.noreply.github.com>
2022-02-07 07:54:17 +0100

    update

--- _build/jupyter_execute/Datasets.py
+++ _build/jupyter_execute/Datasets.py
@@ -3,8 +3,10 @@
 
 # # Datasets
 # 
-# 
+# Github: https://github.com/AGHdMData/Data-Analytics</br>
+# </br>
 # Regression --> Housing.csv (Github) </br>
 # Classification --> ParisHousingClass.csv (Github)</br>
 # Clustering --> Spending_Score.csv (Github)</br>
-# ## What is MyST?
+# </br>
+# Links zu den Original Datasets in den Referenzen.
